0221b966a14c0f2c59ffcb51209a5416679c6b13
jakechamplain <jacob.wright@mymail.champlain.edu>
2018-01-22 12:09:28 +0000

    Add files via upload

---
+++ Search.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Search {
+	private Node root;
+	private Node temp;
+	private State goal;
+	
+	public Search(State stateZero) {
+		root = new Node(stateZero);
+		temp = root;
+	}
+
+	
+	public void search1() {
+		
+	}
+}
