3a645e02367e8a8e7fd28f276733844ae0f7d6f5
jakechamplain <jacob.wright@mymail.champlain.edu>
2018-01-25 20:39:06 +0000

    Update NewAgent.java

--- NewAgent.java
+++ NewAgent.java
@@ -131,7 +131,7 @@ int[][] d_coor;
 			State initialState = new State(false, home_point, orientation, d_coor, initialEnv); //atDirt
 			Search search = new Search(initialState);
 			System.out.println("*************************************************");
-			//ArrayList<String> path1 = search.breadthFirstSearch();
+			ArrayList<String> path1 = search.breadthFirstSearch();
 	    }
 
 	    public String nextAction(Collection<String> percepts) {
